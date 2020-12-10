5afe12f65b006185f61deb8d45c21b65169e272e
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2020-12-11 02:04:54 +0300

    fixed worker not async function

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 const {parentPort} = require('worker_threads');
 const {MESSAGE_CHANNEL} = require('./constants');
 
-parentPort.on(MESSAGE_CHANNEL, (args) => {
+parentPort.on(MESSAGE_CHANNEL,async (args) => {
     // Require and call the function for this specific task
     var result = require(args.filePath)[args.functionName](...args.params);
 
