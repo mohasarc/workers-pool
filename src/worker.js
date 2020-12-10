022c7067cb38761dab594d46560d8c37f8708b85
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2020-12-11 01:41:05 +0300

    handled promises in worker

--- src/worker.js
+++ src/worker.js
@@ -5,6 +5,15 @@ parentPort.on(MESSAGE_CHANNEL, (args) => {
     // Require and call the function for this specific task
     var result = require(args.filePath)[args.functionName](...args.params);
 
+    // If the result is a Promise resolve it
+    if ( Promise.resolve(result) == result) {
+        try{
+            result = await result;
+        } catch(error){
+            result = error;
+        }
+    }
+
     // Prepare the return object
     returnMessage = {
         result : result,
