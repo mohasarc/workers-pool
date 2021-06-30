6846625fa6fef73ee08ec316f86bafe8572ac4c1
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-07-01 01:56:46 +0300

    fixed the MaxListenersExceededWarning: Possible EventEmitter memory leak detected. problem + cleaned the code

--- src/TaskWorker.js
+++ src/TaskWorker.js
@@ -4,49 +4,70 @@ module.exports = class TaskWorker extends Worker{
     constructor(fileName, options){
         super(fileName, options);
         this.busy = false;
-    }
-
-    setBusy(busy){
-        this.busy = busy;
+        this.task;
+        this.rejectCallback;
+        this.resolveCallback;
+        this.initListeners();
     }
 
     isBusy(){
         return this.busy;
     }
 
+    initListeners() {
+        super.on("error", (error) => {
+            this.rejectCallback({task: this.task, worker: this, error});
+            this.clenUp();
+        });
+
+        super.on("exit", (exitCode) => {
+            this.rejectCallback({task: this.task, worker: this, error: new Error("TaskWorker exited with code: ", exitCode)});
+            this.clenUp();
+        });
+
+        super.on("messageerror", (error) => {
+            this.rejectCallback({task: this.task, worker: this, error});
+            this.clenUp();
+        });
+
+        super.on("online", () => {
+            // console.log("TaskWorker is online");
+            this.clenUp();
+        });
+
+        super.on("message", (response) => {
+            if (response.type == 'success')
+                this.resolveCallback({task: this.task, worker: this, result: response.value});
+            else if (response.type == 'error')
+                this.rejectCallback({task: this.task, worker: this, result: response.value});
+
+            this.clenUp();
+        });
+    }
+
     async processTask(task){
         // Build the message object
+        this.busy = true;
+        this.task = task;
         var message = {
             filePath : task.filePath,
             functionName : task.functionName,
             params : task.params,
         }
 
-        super.postMessage(message);
-
-        return new Promise((resolve, reject) => {
-            super.on("error", (error) => {
-                reject({task, worker: this, error});
-            });
-
-            super.on("exit", (exitCode) => {
-                reject({task, worker: this, error: new Error("TaskWorker exited with code: ", exitCode)});
-            });
-
-            super.on("messageerror", (error) => {
-                reject({task, worker: this, error});
-            });
-
-            super.on("online", () => {
-                // console.log("TaskWorker is online");
-            });
-
-            super.on("message", (response) => {
-                if (response.type == 'success')
-                    resolve({task, worker: this, result: response.value});
-                else if (response.type == 'error')
-                    reject({task, worker: this, result: response.value});
-            });
+        let promise = new Promise((resolve, reject) => {
+            this.resolveCallback = resolve;
+            this.rejectCallback = reject;
         });
+
+        super.postMessage(message);
+        return promise;
+    }
+
+    clenUp() {
+        this.task = null;
+        this.rejectCallback = null;
+        this.resolveCallback = null;
+        this.busy = false;
     }
 }
