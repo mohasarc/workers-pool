b0a25cf7e025266a15a26087846b60a19bd9dded
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2021-07-27 23:07:13 +0300

    editted pool init test

--- lib/tests/Pool.test.ts
+++ lib/tests/Pool.test.ts
@@ -2,6 +2,6 @@ import {Pool} from '../src/Pool';
 
 test('Instantiating pool object', ()=>{
     let testPool = new Pool({threadCount: 5});
-    expect(testPool).toBeTruthy();
+    expect(testPool).toBeInstanceOf(Pool);
     testPool.terminate(true);
 });
